5ea2c55d61df6007e48f246de6a2d0881b7dd879
Martin <s_dehlan@stud.hwr-berlin.de>
2016-09-06 10:18:55 +0200

    ng-app binding dont work now

---
+++ scripts/routes.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Martin on 06.09.2016.
+ */
+//routes
+
+var app = angular.module('app', ['ngRoute']);
+
+// configure our routes
+app.config(function($routeProvider) {
+    $routeProvider
+
+    // route for the home page
+        .when('/', {
+            templateUrl : "../pages/home.html",
+            controller  : 'mainController'
+        })
+
+        .when('#', {
+            templateUrl : '../pages/home.html',
+            controller  : 'mainController'
+        })
+
+        // route for the about page
+        .when('/create', {
+            templateUrl : '../pages/create.html',
+        })
+        //route for the login page
+
+        .when('/verwalten', {
+            templateUrl : '../pages/verwalten.html',
+            controller  : 'mainController'
+        })
+
+
+});
